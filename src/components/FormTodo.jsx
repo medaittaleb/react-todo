dc2bf8775cb24fab4a04ad360d3b9999ae3ee5eb
Mohamed Ait Taleb <yamixlive@gmail.com>
2023-01-17 20:51:39 +0100

    form style

--- src/components/FormTodo.jsx
+++ src/components/FormTodo.jsx
@@ -1,13 +1,48 @@
 import React, { useState } from 'react'
 
 export default function FormTodo() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(
+    [
+      {
+        "id": crypto.randomUUID(),
+        "title": "delectus aut autem",
+        "completed": false
+      },
+      {
+        "id": crypto.randomUUID(),
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+      },
+      {
+        "id": crypto.randomUUID(),
+        "title": "fugiat veniam minus",
+        "completed": false
+      },
+      {
+        "id": crypto.randomUUID(),
+        "title": "et porro tempora",
+        "completed": true
+      },
+      {
+        "id": crypto.randomUUID(),
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+      }
+    ]
+  );
   const [input, setInput] = useState("");
 
   const addTodo = (todo) => {
+
+    if (todo === "") {
+      alert('Add a Todo Title ');
+      return;
+    }
+
     const newTodo = {
-      id: Math.random(),
-      todo: todo,
+      id: crypto.randomUUID(),
+      title: todo,
+      completed: false
     };
 
     // add the todo to the list
@@ -24,33 +59,49 @@ export default function FormTodo() {
     setList(newList);
   };
 
+  const doneit = (id) => {
+    const newList = list.map(todo => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+
+    setList(newList);
+  };
+
+  
+
   return (
     <div className='wrapper'>
-        <div id="myDIV" class="header">
+        <div className="header">
             <h2>My To Do List</h2>
-            <input  type="text" 
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                    placeholder="Title..." 
+            <input className='newtitle'
+              type="text" 
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Title..." 
             />
             <span 
-                  onClick={() => addTodo(input)} 
-                  class="addBtn"
-                >Add
+              onClick={() => addTodo(input)} 
+              className="addBtn"
+              > Add
             </span>
         </div>
 
-        <ul>
+        <div className="list-todos">
+          <ul 
+            className='todos'>
             {list.map((todo) => (
               <li 
                 key={todo.id} 
-                className="checked"
+                className={`todo ${todo.completed ? "checked" : ""}`}
+                onClick={() => doneit(todo.id)}
                 >                
-                {todo.todo}
-                <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+                  {todo.title}
+                <button className='remv-todo' onClick={() => deleteTodo(todo.id)}>&times;</button>
               </li>
-            ))}
-        </ul>
+              ))}
+          </ul>
+        </div>
+
     </div>
   )
 }
